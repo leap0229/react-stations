b7e6dcb87ffb5d727a3137fe16a2c8c3b13fa9cf
leap0229 <tomoya0229@gmail.com>
2021-06-01 21:52:17 +0900

    complete station2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import * as React from 'react'
 import './App.css'
 
 /**
- * 
+ *
  * @type {React.FC}
  */
 export const App = () => {
   return (
-    <div>
-      <h2>Hello, world!</h2>
-    </div>
+    <>
+      <header>Dog App</header>
+      <div>
+        <h2>Hello, world!</h2>
+      </div>
+    </>
   )
 }
