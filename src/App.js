8bd6b0c6a9e79c590635508bd350236acd457562
leap0229 <tomoya0229@gmail.com>
2021-06-01 22:10:34 +0900

    complete station6

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const DOGAPIURL = 'https://dog.ceo/api/breeds/image/random'
+
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/germanshepherd/n02106662_597.jpg',
   )
 
-  const updateDogUrl = () => {
-    setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')
+  const updateDogUrl = async () => {
+    const res = await fetch(DOGAPIURL)
+    const { message } = await res.json()
+    setDogUrl(message)
   }
 
   return (
